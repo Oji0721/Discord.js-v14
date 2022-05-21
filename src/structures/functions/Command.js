f0e068d779e2ea61dd73a1f918c02b144dc6f8cf
Aldous User <>
2022-05-21 10:51:47 +0000

    Hello

--- src/structures/functions/Command.js
+++ src/structures/functions/Command.js
@@ -1,15 +1,15 @@
-const { ChatInputCommandInteraction, ContextMenuCommandInteraction, ApplicationCommandData, PermissionsBitField } = require('discord.js');
+const { ChatInputCommandInteraction, ContextMenuCommandInteraction, ApplicationCommandData, PermissionsBitField, GuildMember } = require('discord.js');
 const { WbotClient } = require('../client/WbotClient');
 
 /**
-* @typedef {{ client: WbotClient, interaction: ChatInputCommandInteraction | ContextMenuCommandInteraction, args: String[] }} RunOptions
+* @typedef {{ client: WbotClient, interaction: ChatInputCommandInteraction | ContextMenuCommandInteraction & { member: GuildMember }, args: String[] }} RunOptions
 * @param {RunOptions} runOptions
 */
 function RunFunction(runOptions) {}
 
 class Command {
   /**
-  * @typedef {{ cooldown?: Number, userPermissions?: PermissionsBitField, options?: ApplicationCommandOptionData, run: RunFunction } & ApplicationCommandData} CommandOptions
+  * @typedef {{ cooldown?: Number, userPermissions?: PermissionsBitField, run: RunFunction } & ApplicationCommandData } CommandOptions
   * @param {CommandOptions} options
   */
   constructor(options) {
