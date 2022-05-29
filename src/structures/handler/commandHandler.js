6a5e9b547beda4961d210042f390a0630585bcaa
Oji0721 <90021248+Oji0721@users.noreply.github.com>
2022-05-29 21:12:39 +0900

    Format the code

--- src/structures/handler/commandHandler.js
+++ src/structures/handler/commandHandler.js
@@ -17,12 +17,12 @@ module.exports = async (client, globPromise, ascii) => {
     const file = require(value);
     const L = value.split('/');
     
-if (!file?.name) {
-  return table.addRow(L[7], '❌');
-} else {
-  client.commands.set(file.name, file);
-  table.addRow(L[7], '✅');
-};
+    if (!file?.name) {
+      return table.addRow(L[7], '❌');
+    } else {
+      client.commands.set(file.name, file);
+      table.addRow(L[7], '✅');
+    };
 
     if ([2, 3].includes(file.type)) delete file.description;
     arrayOfSlashCommands.push(file)
@@ -32,14 +32,18 @@ if (!file?.name) {
     (async () => {
       try {
         if (process.env.GUILD_ID) {
-          await rest.put(Routes.applicationGuildCommands(client.user.id, process.env.GUILD_ID), { body: arrayOfSlashCommands });
+          await rest.put(
+            Routes.applicationGuildCommands(client.user.id, process.env.GUILD_ID),
+            { body: arrayOfSlashCommands });
         } else {
-          await rest.put(Routes.applicationCommands(client.user.id), { body: arrayOfSlashCommands });
+          await rest.put(
+            Routes.applicationCommands(client.user.id),
+            { body: arrayOfSlashCommands });
         }
       } catch (err) {
         console.log(err)
       }
     })();
   });
-    console.log(table.toString());
+  console.log(table.toString());
 }
