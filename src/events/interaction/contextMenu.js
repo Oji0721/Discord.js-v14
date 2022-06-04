e5b30fe592458edf5c90038de20510730f49fbac
Oji0721 <90021248+Oji0721@users.noreply.github.com>
2022-06-04 16:55:39 +0900

    Format the code

--- src/events/interaction/contextMenu.js
+++ src/events/interaction/contextMenu.js
@@ -1,9 +1,9 @@
 const { Event } = require('../../structures/functions/Event');
 
 module.exports = new Event('interactionCreate', async(client, interaction) => {
-    if (inter.isContextMenuCommand()) {
-      const cmd = client.commands.get(interaction.commandName);
-      
-if (cmd) cmd.run({ interaction, client });
-    }
-}
+  if (interaction.isContextMenuCommand()) {
+    const cmd = client.commands.get(interaction.commandName);
+    
+    if (cmd) cmd.run({ interaction, client });
+  }
+})
