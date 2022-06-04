dc5b31c1b6646351dcd4d6bd6856b6e0b544edcc
Oji0721 <90021248+Oji0721@users.noreply.github.com>
2022-06-04 16:46:25 +0900

    Add a formater

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -2,13 +2,13 @@ const { Event } = require('../../structures/functions/Event');
 const { connect } = require('mongoose');
 require('dotenv').config();
 
-module.exports = new Event('ready', async(client) => {
-    await client.application.fetch();
-    
-    console.log(`${client.user.tag} is ready to go!`);
-    if (process.env.MONGO_URL) {
-      await connect(process.env.MONGO_URL)
-        .then(() => console.log('Connected to mongodb'))
-        .catch((err) => console.log(err));
-    }
+module.exports = new Event('ready', async (client) => {
+  await client.application.fetch();
+  
+  console.log(`${client.user.tag} is ready to go!`);
+  if (process.env.MONGO_URL) {
+    await connect(process.env.MONGO_URL)
+      .then(() => console.log('Connected to mongodb'))
+      .catch((err) => console.log(err));
+  }
 })
