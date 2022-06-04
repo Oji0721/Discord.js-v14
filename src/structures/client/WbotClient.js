9ec12c79e0c8a6943b7af2fe2a1ef3c7c30ddf0d
Oji0721 <90021248+Oji0721@users.noreply.github.com>
2022-06-04 23:27:03 +0900

    Update WbotClient.js

--- src/structures/client/WbotClient.js
+++ src/structures/client/WbotClient.js
@@ -1,5 +1,4 @@
 const { Client, Collection } = require('discord.js');
-const { DiscordTogether } = require('discord-together');
 require('dotenv').config();
 
 class WbotClient extends Client {
@@ -9,13 +8,11 @@ class WbotClient extends Client {
       intents: 131071
     });
     this.commands = new Collection();
-    this.guildCommands = new Collection();
     this.snipes = new Collection();
     this.cooldown = new Collection();
     this.config = require("../config/config.json");
     this.owners = this.config.owners;
     this.color = this.config.color;
-    this.discordTogether = new DiscordTogether(this);
   }
   start() {
     if(!process.env.DISCORD_TOKEN) return console.log('❌ Please provide a DISCORD_TOKEN and put it in .env file');
