b03bc4764223b732123fb6395b90e92e2760a5b4
Oji0721 <90021248+Oji0721@users.noreply.github.com>
2022-06-04 16:07:30 +0900

    Update contextMenu.js

--- src/events/interaction/contextMenu.js
+++ src/events/interaction/contextMenu.js
@@ -1,10 +1,9 @@
-module.exports = {
-  name: 'interactionCreate',
-  async execute(interaction, client) {
+const { Event } = require('../../structures/functions/Event');
+
+module.exports = new Event('interactionCreate', async(client, interaction) => {
     if (inter.isContextMenuCommand()) {
       const cmd = client.commands.get(interaction.commandName);
       
 if (cmd) cmd.run({ interaction, client });
     }
-  }
 }
