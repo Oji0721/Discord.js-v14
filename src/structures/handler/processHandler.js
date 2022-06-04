1ac80d0b16a3ac9cb0dff52f3e3510e6563fd2ad
Oji0721 <90021248+Oji0721@users.noreply.github.com>
2022-06-04 20:32:50 +0900

    Update processHandler.js

--- src/structures/handler/processHandler.js
+++ src/structures/handler/processHandler.js
@@ -2,4 +2,16 @@ module.exports = () => {
   process.on('unhandledRejection', (reason, p) => {
     console.log(reason, p);
   });
+  
+  process.on('uncaughtException', (err, origin) => {
+    console.log(err, origin);
+  });
+
+  process.on('uncaughtExceptionMonitor', (err, origin) => {
+    console.log(err, origin);
+  });
+
+  process.on('multipleResolves', (type, promise, reason) => {
+    console.log(type, promise, reason);
+  });
 }
